6249454ace243d3728c37209da8b54dc6e35e7b1
Juan Plaza <jufeplasa@hotmail.com>
2024-03-21 10:01:50 -0500

    add code of random matrix generator

--- MultiplicacionMatriz/src/model/RandomMatrixGenerator.java
+++ MultiplicacionMatriz/src/model/RandomMatrixGenerator.java
@@ -1,5 +1,21 @@
 package model;
-
+import java.util.Random;
 public class RandomMatrixGenerator {
-
+	
+	public RandomMatrixGenerator() {
+		
+	}
+	
+	public int[][] generateRandomMatrix(int rows, int columns){ 
+	
+	 int[][] matrix = new int[rows][columns];
+	 Random random = new Random();
+	 for (int i = 0; i < rows; i++) {
+		 for (int j = 0; j < columns; j++) {
+			 //Números aleatorios entre 0 y 99
+			 matrix[i][j] = random.nextInt(100); 
+		 }
+	 }
+	 return matrix;
+	 }
 }
