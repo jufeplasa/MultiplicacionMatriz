27f3208ebead7cc356dd546c133102818cc2eff6
Juan Plaza <jufeplasa@hotmail.com>
2024-03-21 10:53:19 -0500

    add code for main

--- MultiplicacionMatriz/src/ui/Main.java
+++ MultiplicacionMatriz/src/ui/Main.java
@@ -1,7 +1,25 @@
 package ui;
-
+import model.*;
 public class Main {
+	
+	private static SequentialMatrixMultiplication smm; 
+	private static ThreadPoolMatrixMultiplication tmm; 
+	private static RandomMatrixGenerator rmg; 
+	
 	public static void main(String[] arg) {
+		smm = new SequentialMatrixMultiplication();
+		tmm = new ThreadPoolMatrixMultiplication();
+		rmg = new RandomMatrixGenerator();
+		
+		int [][] matrizA= rmg.generateRandomMatrix(2, 2);
+		int [][] matrizB= rmg.generateRandomMatrix(2, 2);
+		smm.multiply(matrizA, matrizB);
+		int [][] matrizR=smm.getResult();
+		
+		
+	}
+	
+	public void showMatriz(int [][] matriz) {
 		
 		
 	}
